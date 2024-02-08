0a2c0f7f747043330a07b399b4697250907ef3e9
Bruno César <bcgmeireles@gmail.com>
2024-02-08 11:38:00 -0300

    feat: finishing html

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  
+
+  cardImages: string[] = [
+    './assets/card.png', './assets/card.png', './assets/card.png', './assets/card.png', './assets/card.png', './assets/card.png', './assets/card.png', './assets/card.png', './assets/card.png', './assets/card.png',
+  ];
+
+  indiceAtual: number = 0;
+  imagensPorPagina: number = 4;
+
+  getImagensExibicao(): string[] {
+    const indiceInicial = this.indiceAtual;
+    const indiceFinal = indiceInicial + this.imagensPorPagina;
+    return this.cardImages.slice(indiceInicial, indiceFinal);
+  }
+
+  previous() {
+    if(this.indiceAtual > 0) {
+      this.indiceAtual = (this.indiceAtual - this.imagensPorPagina + this.cardImages.length) % this.cardImages.length;
+    }
+  }
+
+  next() {
+    if(this.indiceAtual < (this.cardImages.length - this.imagensPorPagina)) {
+      this.indiceAtual = (this.indiceAtual + this.imagensPorPagina) % this.cardImages.length;
+    }
+  }
 }
