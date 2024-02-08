12bae7cb4a5fa83fc91d78b2369da9e1da4bb1ed
Bruno César <bcgmeireles@gmail.com>
2024-02-07 21:29:10 -0300

    fix: adjustments in angular.json

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-WRGX5IIA.mjs";import"./chunk-2TEQDHCH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BPBIT5BK.mjs";import"./chunk-2TEQDHCH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
